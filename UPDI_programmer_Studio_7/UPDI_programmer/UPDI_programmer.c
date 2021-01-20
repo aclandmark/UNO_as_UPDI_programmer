20fe8d186f96f7e8763480f6b5ddab777eb8f18d
aclandmark <mark.osborne15@btinternet.com>
2021-01-20 16:16:52 +0000

    Create UPDI_programmer.c

---
+++ UPDI_programmer_Studio_7/UPDI_programmer/UPDI_programmer.c
@@ -0,0 +1,18 @@
+/*
+ * UPDI_programmer.c
+ *
+ * Created: 20/01/2021 16:07:45
+ * Author : Mark
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
