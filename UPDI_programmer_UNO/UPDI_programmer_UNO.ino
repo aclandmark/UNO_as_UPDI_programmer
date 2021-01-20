0223cd2fed1993a097bdf7cfdebff2bdf7f1ab08
aclandmark <mark.osborne15@btinternet.com>
2021-01-20 17:18:44 +0000

    Update UPDI_programmer_UNO.ino

--- UPDI_programmer_UNO/UPDI_programmer_UNO.ino
+++ UPDI_programmer_UNO/UPDI_programmer_UNO.ino
@@ -1,9 +1,20 @@
-void setup() {
-  // put your setup code here, to run once:
+/*
+ * UPDI_programmer.c
+ *
+ * Created: 20/01/2021 16:07:45
+ * Author : Mark
+ */ 
 
-}
+#include "Project_header_file.h"
 
-void loop() {
-  // put your main code here, to run repeatedly:
 
+int main(void)
+{
+    setup_328_HW;
+  
+  User_prompt;
+  
+  sendString("\r\nUPDI development program");
+  
+  while(1);
 }
