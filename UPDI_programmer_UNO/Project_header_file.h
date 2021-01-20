c0b97bb65f432d84049661284647aa61313a43ae
aclandmark <mark.osborne15@btinternet.com>
2021-01-20 17:19:09 +0000

    Create Project_header_file.h

---
+++ UPDI_programmer_UNO/Project_header_file.h
@@ -0,0 +1,16 @@
+
+#include <avr/io.h>
+#include <stdlib.h>
+#include <avr/interrupt.h>
+#include <avr/wdt.h>
+
+
+
+#include "Resources\UNO_AVR_Programmer.h"
+#include "Resources\Device_characteristics.c"
+#include "Resources\UNO_AVR_Programmer_HW_subs.c"
+#include "Resources\Basic_IO_and_Timer_subs.c"
+#include "Resources\ASKII_subroutines.c"
+#include "Resources\Hex_Programmer_subs.c"
+
+#include "Resources\Hex_Programmer.c"
