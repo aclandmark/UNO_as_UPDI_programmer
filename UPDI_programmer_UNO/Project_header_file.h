d28ba54e310ddb526d521c7860f252f251eff92b
aclandmark <mark.osborne15@btinternet.com>
2021-01-27 11:39:36 +0000

    Update Project_header_file.h

--- UPDI_programmer_UNO/Project_header_file.h
+++ UPDI_programmer_UNO/Project_header_file.h
@@ -7,10 +7,23 @@
 
 
 #include "Resources\UNO_AVR_Programmer.h"
-#include "Resources\Device_characteristics.c"
-#include "Resources\UNO_AVR_Programmer_HW_subs.c"
 #include "Resources\Basic_IO_and_Timer_subs.c"
 #include "Resources\ASKII_subroutines.c"
-#include "Resources\Hex_Programmer_subs.c"
 
-#include "Resources\Hex_Programmer.c"
+//#include "Resources\Hex_Programmer_subs.c"
+//#include "Resources\Hex_Programmer.c"
+//#include "Resources\Device_characteristics.c"
+//#include "Resources\UNO_AVR_Programmer_HW_subs.c"
+
+
+#define delay_of_0_25uS       asm("nop"); asm("nop"); asm("nop"); asm("nop");
+#define delay_of_0_75uS       delay_of_0_25uS; delay_of_0_25uS; delay_of_0_25uS;
+#define out_h                 PORTC |= (1 << PORTC0)
+#define out_l                 PORTC &= (~(1 << PORTC0))
+
+#define clock_delay_T         Timer_T0_sub(T0_delay_200us)
+
+#define input_h               (PINC & (1 << PINC0))
+#define input_l               (!(PINC & (1 << PINC0)))
+#define clock_delay_R         Timer_T0_sub(T0_delay_200us)
+#define half_clock_delay_R    Timer_T0_sub(T0_delay_100us)
