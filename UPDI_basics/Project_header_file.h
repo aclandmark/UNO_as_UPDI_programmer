f31ba25ff312f8cd35ed4477e21720915dd3ed04
aclandmark <mark.osborne15@btinternet.com>
2021-02-05 15:23:54 +0000

    Update Project_header_file.h

--- UPDI_basics/Project_header_file.h
+++ UPDI_basics/Project_header_file.h
@@ -17,20 +17,20 @@
 //#include "Resources\UNO_AVR_Programmer_HW_subs.c"
 
 
-#define delay_of_0_25uS       asm("nop"); asm("nop"); asm("nop"); asm("nop");
-#define delay_of_0_75uS       delay_of_0_25uS; delay_of_0_25uS; delay_of_0_25uS;
-#define out_h                 PORTC |= (1 << PORTC0)
-#define out_l                 PORTC &= (~(1 << PORTC0))
+#define delay_of_0_25uS             asm("nop"); asm("nop"); asm("nop"); asm("nop");
+#define delay_of_0_75uS             delay_of_0_25uS; delay_of_0_25uS; delay_of_0_25uS;
+#define out_h                       PORTC |= (1 << PORTC0)
+#define out_l                       PORTC &= (~(1 << PORTC0))
 
-//#define clock_delay_T         Timer_T0_sub(T0_delay_200us)
-#define clock_delay_T         Timer_T0_sub(T0_delay_40us)
-//#define clock_delay_T         Timer_T0_sub(T0_delay_20us)
 
-#define input_h               (PINC & (1 << PINC0))
-#define input_l               (!(PINC & (1 << PINC0)))
-//#define clock_delay_R         Timer_T0_sub(T0_delay_200us)
-//#define half_clock_delay_R    Timer_T0_sub(T0_delay_100us)
-#define clock_delay_R         Timer_T0_sub(T0_delay_40us)
-#define half_clock_delay_R    Timer_T0_sub(T0_delay_20us)
-//#define clock_delay_R         Timer_T0_sub(T0_delay_20us)
-//#define half_clock_delay_R    Timer_T0_sub(T0_delay_10us)
+#define clock_delay_T               Timer_T0_sub(T0_delay_20us) 
+#define clock_delay_R               Timer_T0_sub(T0_delay_20us)
+#define half_clock_delay_R          Timer_T0_sub(T0_delay_10us)
+
+
+#define clock_delay_T_upload         Timer_T0_sub(T0_delay_10us) 
+#define clock_delay_R_upload         Timer_T0_sub(T0_delay_11us)
+#define half_clock_delay_R_upload    Timer_T0_sub(T0_delay_5_5us)
+
+#define input_h                     (PINC & (1 << PINC0))
+#define input_l                     (!(PINC & (1 << PINC0)))
