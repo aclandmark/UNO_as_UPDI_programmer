022f320270423161806a2a8abd094a25cf8afab8
aclandmark <mark.osborne15@btinternet.com>
2021-03-05 10:42:34 +0000

    Create Resources_UNO_UPDI_IP_V4.h

---
+++ UNO_UPDI_IP_V4/Resources_UNO_UPDI_IP_V4.h
@@ -0,0 +1,16 @@
+
+#include <avr/io.h>
+#include <stdlib.h>
+#include <avr/interrupt.h>
+#include <avr/wdt.h>
+#include <avr/eeprom.h>
+
+
+
+#include "Resources_UNO_UPDI_IP_V4\UPDI_Programmer.h"
+#include "Resources_UNO_UPDI_IP_V4\Basic_IO_and_Timer_subs.c"
+#include "Resources_UNO_UPDI_IP_V4\ASKII_subroutines.c"
+#include "Resources_UNO_UPDI_IP_V4\Hex_Programmer_subs.c"
+#include "Resources_UNO_UPDI_IP_V4\Hex_Programmer.c"
+#include "Resources_UNO_UPDI_IP_V4\UPDI_subroutines.c"
+#include "Resources_UNO_UPDI_IP_V4\verify_flash.c"
