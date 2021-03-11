3780f60c173de7a33456f427d07b426643b285a4
aclandmark <mark.osborne15@btinternet.com>
2021-03-11 16:52:15 +0000

    Create Resources_8MHz_V2.h

---
+++ UNO_UPDI_8MHz_V2/Resources_8MHz_V2.h
@@ -0,0 +1,16 @@
+
+#include <avr/io.h>
+#include <stdlib.h>
+#include <avr/interrupt.h>
+#include <avr/wdt.h>
+#include <avr/eeprom.h>
+
+
+
+#include "Resources_8MHz_V2\UPDI_Programmer.h"
+#include "Resources_8MHz_V2\Basic_IO_and_Timer_subs.c"
+#include "Resources_8MHz_V2\ASKII_subroutines.c"
+#include "Resources_8MHz_V2\Hex_Programmer_subs.c"
+#include "Resources_8MHz_V2\Hex_Programmer.c"
+#include "Resources_8MHz_V2\UPDI_subroutines.c"
+#include "Resources_8MHz_V2\verify_flash.c"
