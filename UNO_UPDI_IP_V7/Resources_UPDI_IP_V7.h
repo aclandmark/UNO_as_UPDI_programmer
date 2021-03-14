e00add5b7fe7a22595daecfc6c47a2c9735f6981
aclandmark <mark.osborne15@btinternet.com>
2021-03-14 12:06:56 +0000

    Create Resources_UPDI_IP_V7.h

---
+++ UNO_UPDI_IP_V7/Resources_UPDI_IP_V7.h
@@ -0,0 +1,16 @@
+
+#include <avr/io.h>
+#include <stdlib.h>
+#include <avr/interrupt.h>
+#include <avr/wdt.h>
+#include <avr/eeprom.h>
+
+
+
+#include "Resources_UPDI_IP_V7\UPDI_Programmer.h"
+#include "Resources_UPDI_IP_V7\Basic_IO_and_Timer_subs.c"
+#include "Resources_UPDI_IP_V7\ASKII_subroutines.c"
+#include "Resources_UPDI_IP_V7\Hex_Programmer_subs.c"
+#include "Resources_UPDI_IP_V7\Hex_Programmer.c"
+#include "Resources_UPDI_IP_V7\UPDI_subroutines.c"
+#include "Resources_UPDI_IP_V7\verify_flash.c"
