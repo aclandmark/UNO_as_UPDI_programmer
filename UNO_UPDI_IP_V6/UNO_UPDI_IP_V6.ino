753fc2da4c6031ec0e8aab304d5e273ea5896f72
aclandmark <mark.osborne15@btinternet.com>
2021-03-14 07:34:09 +0000

    Update UNO_UPDI_IP_V6.ino

--- UNO_UPDI_IP_V6/UNO_UPDI_IP_V6.ino
+++ UNO_UPDI_IP_V6/UNO_UPDI_IP_V6.ino
@@ -1,33 +1,15 @@
-/*
- Programming AVR devices with a UPDI interface
- Arduino sketch to be downloaded to a UNO
- Open a terminal program before running the shetch.
- Set terminal to 38400Hz, 8 data bits, 1 stop bits, no parity and no handshaking
- User prompt R R R..... is generated
- Press R and then respond to the on-screen instructions
- If the "compiled binary file" is exported (see Shetch drop down menue)
- it can be copied to an Atmega328 using the UNO AVR Programmer.
- The only difference is that the baud rate must be reduced to 19.2K
- (Note: The internal RC clock may also need calibrating but this can usually
- be avoided if several devices are available.)
-
- This Shetch has only been tested with ATtiny 806/1606 both excellent devices for the
- newcommer: They are available in a SOIC package that can be hand soldered and they have 
- hardware I2C and UART modules rather the the USI that is software driven. 
- Test projects have been written for the ATtiny1606 using Atmel Studio 7.
- 
-Note:  It is hoped that any newcommer persuing this project will find both Arduino and  
-Studio 7 equally invaluable when attempting to build projects in the easiest and most flexible way.
-Programmers note pad is also highly recommended for viewing C-files that Arduino can't open easily.  
- */
-
+//All test files programmed OK at 38.4KHz  Fills 16K flash in 16 seconds.
+//FINAL UNO VERSION Break subroutine works reliably
+//Odd record length dealt with
 
 
 #include "Resources_UPDI_IP_V6.h"
 
 int main (void){ 
 char kepress;
-setup_328_HW;                                           //see "Resources\ATMEGA_Programmer.h"
+//CLKPR = (1 << CLKPCE);
+//CLKPR = (1 << CLKPS0);
+setup_328_HW;                                                     //see "Resources\ATMEGA_Programmer.h"
 P_counter = 0;
 User_prompt;
 
@@ -39,24 +21,21 @@ waitforkeypress();
 Timer_T1_sub_with_interrupt(T1_delay_100ms);
 sei();
 contact_target;
-configure_updi;                                       //Increases UPDI clock to 16MHz and reduce guard band to 2 cycles
+configure_updi;                              //Increase UPDI clock to 16MHz and reduce guard band to 2 cycles
 print_out_SIB;
-if(UPDI_timeout)
-{sendString("Power Cycle Required\r\n");
-while(1);}
-else{cli();TCCR1B = 0;
-TIMSK1 &= (~(1 << TOIE1));}
+if(UPDI_timeout){sendString("Power Cycle Required\r\n");while(1);}
+else{cli();TCCR1B = 0;TIMSK1 &= (~(1 << TOIE1));}
 
 
 initialise_NVM_programming;
 sendString("Programming fuses\r\n");
-write_fuse (WDTCFG, 0x0);                             //Default value: WDT under program control
-write_fuse (BODCFG,0x0);                              //Default value: BOD dissabled
-write_fuse (OSCCFG, 0x7E);                            //Default value: 20MHz internal clock
-write_fuse (SYSCFG0, 0xF7);                           //UPDI enabled, EEPROM preserved at chip erase
-write_fuse (SYSCFG1, 0xFD);                           //16mS SUT
-write_fuse (APPEND, 0x0);                             //Default value: No flash partitions
-write_fuse (BOOTEND, 0x0);                            //Default value: No flash partitions
+write_fuse (WDTCFG, 0x0);           //Default value: WDT under program control
+write_fuse (BODCFG,0x0);            //Default value: BOD dissabled
+write_fuse (OSCCFG, 0x7E);          //Default value: 20MHz internal clock
+write_fuse (SYSCFG0, 0xF7);         //UPDI enabled, EEPROM preserved at chip erase
+write_fuse (SYSCFG1, 0xFD);         //16mS SUT
+write_fuse (APPEND, 0x0);            //Default value: No flash partitions
+write_fuse (BOOTEND, 0x0);          //Default value: No flash partitions
 
 
 
@@ -72,7 +51,7 @@ read_out_fuses();
 
 
 sendString("\r\n\r\nUnlocking device");
-write_fuse (0x128A, 0xC5);                              //Device unlocked
+write_fuse (0x128A, 0xC5);                                        //Device unlocked
 sendString("\r\nY to Lock device or AOK");
 if (waitforkeypress() == 'Y')
 {write_fuse (0x128A, 0x0);
@@ -93,34 +72,26 @@ return 1;}
 
 
 /***************************************************************************************************************************************************/
-ISR(USART_RX_vect){upload_hex();}
+ISR(USART_RX_vect){ upload_hex();}
 ISR(TIMER1_OVF_vect) {UPDI_timeout = 1;TCCR1B = 0;}
 
 
 /***********************************************************************************************/
-
-/*
-Routines used to drive the UART interface when progamming config bytes and for other non-time
-critical operations.  Use of subroutines rather than code definitions make efficient use of program
-memory but is not so good on speed.
- */
-
-
-void UART_Tx(unsigned int data_byte_T){               //starts Hi Z
+void UART_Tx(unsigned int data_byte_T){                                       //starts Hi Z
   unsigned char parity = 0;
-  DDRC |= (1 << DDC0);                                //start bit
+  DDRC |= (1 << DDC0);                                                        //start bit
   clock_delay_T;
 
 for (int n = 0; n <= 7; n++){
   if (data_byte_T & (1 << n))
   {DDRC &= (~(1 << DDC0)); parity += 1;}
- else {DDRC |= (1 << DDC0);}clock_delay_T;}           //clock data out
+ else {DDRC |= (1 << DDC0);}clock_delay_T;}                                   //clock data out
 
 if (parity%2){DDRC &= (~(1 << DDC0));} 
 else {DDRC |= (1 << DDC0);}
-  clock_delay_T;                                      //clock parity bit
-  DDRC &= (~(1 << DDC0));                             //initiate stop bit 
-  clock_delay_T;                                      //Stop bits
+  clock_delay_T;                                                              //clock parity bit
+  DDRC &= (~(1 << DDC0));                                                     //initiate stop bit 
+  clock_delay_T;                                                              //Stop bits
   clock_delay_T;}
 
 
@@ -132,16 +103,15 @@ unsigned char UART_Rx(void){
   unsigned char data_byte_R = 0;
     char parity = 0;
                           
-  while (PINC & (1 << PINC0));                        //wait for start bit
-  clock_delay_R;                                      //The full delay is required 
+  while (PINC & (1 << PINC0));                                              //wait for start bit
+  clock_delay_R;                                                            //The full delay is required 
   for (int n= 0; n <= 7; n++){clock_delay_R;
-    if (PINC & (1 << PINC0))
-    {data_byte_R |= (1 << n); parity += 1;}}
+    if (PINC & (1 << PINC0)){data_byte_R |= (1 << n); parity += 1;}}
     clock_delay_R;
 
  if ((PINC & (1 << PINC0)) && (parity%2)); 
- else P_counter++;                                      //Parity often received in error?????
+ else P_counter++;                                                            //Parity often received in error?????
  
- clock_delay_R;                                         //stop bit
- clock_delay_R;                                         //stop bit  
+ clock_delay_R;                                                               //stop bit
+ clock_delay_R;                                                               //stop bit  
  return data_byte_R;}
