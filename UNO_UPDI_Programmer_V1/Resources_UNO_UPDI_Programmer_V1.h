c547925de12540f68b78c4120be351cfa9c3148d
aclandmark <mark.osborne15@btinternet.com>
2021-03-25 09:04:19 +0000

    Create Resources_UNO_UPDI_Programmer_V1.h

---
+++ UNO_UPDI_Programmer_V1/Resources_UNO_UPDI_Programmer_V1.h
@@ -0,0 +1,18 @@
+
+#include <avr/io.h>
+#include <stdlib.h>
+#include <avr/interrupt.h>
+#include <avr/wdt.h>
+#include <avr/eeprom.h>
+
+
+
+
+#include "Resources_UNO_UPDI_Programmer_V1\UPDI_Programmer.h"
+#include "Resources_UNO_UPDI_Programmer_V1\Basic_IO_and_Timer_subs.c"
+#include "Resources_UNO_UPDI_Programmer_V1\ASKII_subroutines.c"
+#include "Resources_UNO_UPDI_Programmer_V1\Hex_Programmer_subs.c"
+#include "Resources_UNO_UPDI_Programmer_V1\Hex_Programmer.c"
+#include "Resources_UNO_UPDI_Programmer_V1\UPDI_subroutines.c"
+#include "Resources_UNO_UPDI_Programmer_V1\verify_flash.c"
+#include "Resources_UNO_UPDI_Programmer_V1\Text_Programmer_subs.C"
